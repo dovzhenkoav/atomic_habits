842e5cdb88048381f81045d2d042a08985a3b059
Artemii Dovzhenko <rockenny@ymail.com>
2023-09-27 09:08:46 +0300

    add users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='почта')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
