d575dd498a80e0cc1e302823e3b25d8838a123b5
Artemii Dovzhenko <rockenny@ymail.com>
2023-09-27 11:19:51 +0300

    add registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    permission_classes = [AllowAny,]
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):  #
+        new_user = serializer.save()
+        new_user.set_password(self.request.data['password'])
+        new_user.save()
