3470040ac8772ea65949b5e9abc1b0eef1baaf13
Artemii Dovzhenko <rockenny@ymail.com>
2023-09-27 09:29:26 +0300

    add habit model

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+
+class Habit(models.Model):
+    PERIODICITY_CHOICES = [
+        ('1', 'ежедневно'),
+        ('2', 'через день'),
+        ('3', 'раз в два дня'),
+        ('4', 'раз в три дня'),
+        ('5', 'раз в четыре дня'),
+        ('6', 'раз в пять дней'),
+        ('7', 'раз в шесть дней'),
+    ]
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_by')
+    place = models.CharField(max_length=128, verbose_name='место')
+    time = models.TimeField(verbose_name='время выполнения')
+    action = models.CharField(max_length=128, verbose_name='действие')
+    is_nice = models.BooleanField(verbose_name='признак приятной привычки')
+    related_habit = models.IntegerField(verbose_name='ид связанной привычки')
+    periodicity = models.CharField(max_length=1, choices=PERIODICITY_CHOICES, verbose_name='перидочность')
+    reward = models.CharField(max_length=128, verbose_name='награда')
+    duration = models.CharField(max_length=128, verbose_name='продолжительность')
+    is_public = models.BooleanField(verbose_name='признак публичной привычки')
+
+    def __str__(self):
+        return f'{self.place} {self.time} {self.action} {self.created_by}'
+
+    class Meta:
+        verbose_name = 'привычка'
+        verbose_name_plural = 'привычки'
